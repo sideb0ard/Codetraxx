e1f41d15933a8f04b2047f926d4cd51c2b4ce156
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2013-11-20 22:59:55 -0800

    noise

--- nb0iser.js
+++ nb0iser.js
@@ -6,39 +6,52 @@ var baudio  = require('baudio');
 
 var tau = Math.PI * 2;
 
-var baudio_funct = function() {
+var baudio_funct = function(tickCounter) {
 
   nowplaying = 1;
 
-  var b = baudio(function (tt) {
-    var t = tt % 8;
-    
+  // console.log("TICKCOUNTER:: " + tickCounter);
+  // console.log(typeof tickCounter); 
+
+  var b = baudio(function (tt, tickCounter) {
+    console.log("TICKCOUNTER:: " + tickCounter);
+    donk = (tickCounter / 266666);
+    //console.log("DONK " + donk);
+    //console.log("TT " + tt);
+    // var t = donk % 8;
+    var t = donk % 3;
+    // console.log(typeof tt);
+    //console.log("TT:: " + tt);
+//    var OGt = tt % 8;
+    //var t = tickCounter % 8;
+//    console.log("T:: " + t);
+//    console.log("OGT:: " + OGt);
+
     var n = t % 7;
-    var xs = [ 120, 240, 450, 20 ];
-    
+    // var xs = [ 120, 240, 450, 20 ];
+    var xs = [ 74, 773, 50, 4 ];
+    // var xs = [ 74, tickCounter, 50, 4 ];
+
     var speed = tt % 8 > 7 ? 16 : 2;
     var x = xs[Math.floor(t*speed)%xs.length]
     var z = tt % 8 < 7 ? 1000 : 80;
-    
+
     var f = x + Math.sin(z * (t % 1));
-    
+    // var f = x + Math.sin(z * (t  % 1));
+
     return (
+        // 73 * Math.sin(tau * f)
         0.15 * Math.sin(tau * t * f)
         + 0.1 * Math.sin(tau * t * (f * 2 + 4))
         + (t % (1/2) < 1/24 ? Math.random() : 0)
     );
-    
+
     function sin (x) {
         return Math.sin(tau * t * x);
     }
 });
 
-//  b = baudio(function (t) {
-//      //console.log("ug!");
-//      nowplaying = 0;
-//      return Math.sin(2 * Math.PI * 1760 * t) * (t < 0.1);
-//  })
-  b.play();
+   b.play();
 
 };
 
@@ -46,9 +59,9 @@ codetraxx.subscribe( function(msg) {
   var bpm = msg.bpm, microTick = msg.microTick, tickCounter = msg.tickCounter, beat = msg.beat;
   console.log("BPM: " + bpm + " BEAT: " + beat + " MICROTICK: " + microTick + " TICKCOUNTER: " + tickCounter);
 
-  if (beat === 1 && !nowplaying) {
-   console.log("hola");
-   baudio_funct();
-  }
+  //if (beat === 1 && microTick === 1 && !nowplaying) {
+   //console.log("hola");
+   baudio_funct(tickCounter);
+  //}
 
 });
