2796c54dc2a0fc5743d08ca2a6678dbf5fcd4662
tack <tackyy@gmail.com>
2014-01-10 20:57:35 -0800

    moar cleaning

--- octopus.js
+++ octopus.js
@@ -5,31 +5,28 @@ var codetraxx = require('./codetraxx_lib.js');
 
 var tau = 2 * Math.PI;
 
-var melody = [ 0, -3/2, 1/5, 4/3, 7/5, 0, -5/6, 1/3 ]
-    .map(function (x) { return Math.pow(2, x) })
-;
+var melody = [0, 440, 880, 220, 0, 220, -220, 330].map(function(x) {
+	return Math.pow(2, x)
+});
 
 var bpm = false;
 var firstTick = false;
 
-codetraxx.subscribe( function(msg) {
-	if(!firstTick){
+codetraxx.subscribe(function(msg) {
+	if (!firstTick) {
 		firstTick = msg.tickCounter;
 	}
-	
-	if (msg.beat == 1 && msg.microTick == 1 && !bpm){
-		bpm = parseInt(msg.bpm);
-		var b = baudio(function (t) {
-			// reference:
-			//var m = melody[Math.floor(t * 2 % melody.length)];
-			var m = melody[Math.floor(t * bpm / 55 % melody.length)];
-		    return sin(400 * m);
 
-		    function sin (freq) {
-		        return Math.sin(tau * t * freq);
-		    }
+	if (msg.beat == 1 && msg.microTick == 1 && !bpm) {
+		bpm = parseInt(msg.bpm);
+		var b = baudio(function(t) {
+		var m = melody[Math.floor(t * bpm / 60 % melody.length)];
+		return sin(400 * m) * .1;
+
+		function sin(freq) {
+			return Math.sin(tau * t * freq);
+		}
 		});
 		b.play();
 	}
 });
-
