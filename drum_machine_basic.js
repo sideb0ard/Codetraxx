8d0cf97415a7d723d3fa676bce205afe03d9c10e
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2014-01-15 09:43:20 -0800

    test drum pattern

---
+++ drum_machine_basic.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var codetraxx = require('./codetraxx_lib.js');
+var exec = require('child_process').exec;
+
+var KICK = "wavs/kick.wav";
+var KICK2 = "wavs/KickDrum0016.aif";
+var KICK3 = "wavs/KickDrum0013.aif";
+//var SNARE = "wavs/SnareDrum0012.aif";
+var SNARE = "wavs/Clap.aif";
+
+function playrrr(wav ){
+  exec("play " + wav);
+}
+
+codetraxx.subscribe( function(msg) {
+  var bpm = msg.bpm, microTick = msg.microTick, tickCounter = msg.tickCounter, beat = msg.beat;
+  console.log("BPM: " + bpm + " MICROTICK: " + microTick + " TICK COUNTER: " + tickCounter + " and BEAT is: " + beat);
+
+  if (/[1357]/.test(beat) && microTick == 1) {
+    console.log("MICROTICK IS A " + typeof microTick);
+      playrrr(KICK2);
+  } else if (/[2468]/.test(beat) && microTick == 1) {
+      playrrr(SNARE);
+  }
+});
