541c499cf2c3293f58c60579cace701771946cdc
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2014-03-31 21:53:06 -0700

    Adding arduino/serial capabilities

--- ArduinoiSSSE.js
+++ ArduinoiSSSE.js
@@ -19,29 +19,24 @@ codetraxx.subscribe( 'bpm', function(msg) {
     nowplaying = 1;
     console.log("hola");
     b = baudio(function (t) {
-      //var multiplier = (t / 2) ;
       var multiplier = (t * (bpm / 60)) ;
-      //console.log("T is " + t + " MULTIPLIER is " + multiplier);
-      //
 
-      var n = 47 / t;
-      //var xs = [ 20, 70, 7, 540, n / 300 ];
-      //var xs = [ 40, n / 300, 7 ];
-      var xs = [ 20, 10, 32, 50, 30 ];
-      //var xs = [ 3,3,3,3,3,3,5,3,77777];
-      var x = xs[Math.floor(multiplier*8)%xs.length];
-      var f = x + Math.sin(17 * (n % 7));
-      //return x * sin(f / 13 ) * 0.02;
-      //var vol = "0." + Math.floor(t % 8) + 1;
       var vol = "0." + Math.floor(t % 8) + 2;
-      //console.log("VOL: " + vol);
-      return sin(x) * vol;
+      //return sin(note) * vol;
+      return square(note % 20) * vol;
+      //return saw(note) * vol;
+      //return sin(note % 8) * saw(note % 10) * vol;
 
-      function sin (freq) {
-        //console.log("VOL is " + vol);
-        return Math.sin( tau * multiplier * freq);
+      function sin (x) {
+        return Math.sin( tau * multiplier * x);
       }
-    })
+      function square (x) { 
+        return sin(x) > 0 ? 1 : -1; 
+      }
+      function saw (x) { 
+        return 1-2*(t%(1/x))*x; 
+      }
+    });
 
   b.play();
   }
---
+++ Arduinoid.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var codetraxx = require('./codetraxx_lib.js');
+var baudio  = require('baudio');
+var serialport = require("serialport");
+
+var tau = 2 * Math.PI;
+
+var nowplaying = 0;
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+var sp = serialport.SerialPort;
+var serialPort = new sp("/dev/tty.usbmodem1411", {
+    parser: serialport.parsers.readline("\n")
+    //baudrate: 9600
+});
+
+serialPort.on("open", function () {
+  //console.log('open');
+  serialPort.on('data', function(data) {
+    cleanData = JSON.stringify(data);
+    cleanData.split(",").forEach(function(c) {
+      c = c.replace(/[^0-9\.]+/g, '');
+      var msg = {"lightsensor": c};
+      console.log("Sending msg -- " + JSON.stringify(msg));
+      codetraxx.publish('serial', msg);
+    });
+  });
+});
